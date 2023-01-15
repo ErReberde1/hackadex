f286b603533bc97e4c2867205b18fcce07e8ffd0
Fran <fransanxenxo@gmail.com>
2023-01-15 15:28:05 +0100

    rotate card

--- scripts/main.js
+++ scripts/main.js
@@ -77,6 +77,14 @@ buttonSearch.addEventListener("click", async (e) => {
           </div>
           <div class='cards-back'>
             <img src=${pokemon.sprites.back_default}>
+            <p>Peso: ${pokemon.weight}</p>
+            <p>Altura: ${pokemon.height}</p>
+            <p>Order: ${pokemon.order}</p>
+            <p>Tipo: ${pokemon.types[0].type.name}</p>
+            <p>Ataque: ${pokemon.stats[1].base_stat}</p>
+            <p>Defensa: ${pokemon.stats[2].base_stat}</p>
+            <p>Velocidad: ${pokemon.stats[5].base_stat}</p>
+            <p>Puntos de vida: ${pokemon.types[0].slot}</p>
           </div>
         </div>       
       </article>\n`
