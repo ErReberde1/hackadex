f141d7a1674464ba5643456c044ee075a20ff855
ErReberde <joseluis.gomez.cano@hotmail.es>
2023-01-16 09:27:11 +0100

    Funcionalidad seleccionar card

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
-const articleRight = document.querySelector('.full-card-front');
+
 
 // funcion para seleccionar elementos del DOM
 function $(param) {
   return document.querySelector(param);
+}
+function $All(param) {
+  return document.querySelectorAll(param);
 }
 
 // Función para atacar a la api
@@ -12,15 +15,19 @@ async function atackApi(url) {
   return data
 }
 
+
+
 // Funcion con condiciones para busqueda de pokemon 
 async function conexionApi(URI, namePoke) {
-  try {
 
+  try {
+    if(namePoke == undefined) throw new Error("Escribe algo")
     return await atackApi(URI + namePoke)
 
   } catch (e) {
 
     let { results } = await atackApi("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=2100")
+
     try {
       console.log(namePoke)
       const resultSearch = results.filter(e => e.name.includes(namePoke))
@@ -43,6 +50,30 @@ async function conexionApi(URI, namePoke) {
 
 }
 
+async function selectCard (id){
+  resulTag.innerHTML = ""
+  let pokemon = await atackApi(url+id)
+  resulTag.innerHTML = `<article class="card">
+        <p>Nombre: ${pokemon.name}</p>
+        <p>Peso: ${pokemon.weight}</p>
+        <p>Altura: ${pokemon.height}</p>
+        <p>Order: ${pokemon.order}</p>
+        <p>Tipo: ${pokemon.types[0].type.name}</p>
+        <p>Ataque: ${pokemon.stats[1].base_stat}</p>
+        <p>Defensa: ${pokemon.stats[2].base_stat}</p>
+        <p>Velocidad: ${pokemon.stats[5].base_stat}</p>
+        <p>Puntos de vida: ${pokemon.types[0].slot}</p>
+      </article>`;
+  
+  articleRight.innerHTML = `
+      <h1>Pokemon</h1>
+      <figure>
+          <img src=${pokemon.sprites.front_default}>
+          <img src=${pokemon.sprites.back_default}>
+          </figure>
+      `
+}
+
 
 //Seleccionamos el elemento input/buscador del DOM y lo metemos en la variable
 //  search
@@ -53,42 +84,50 @@ const resultsSection = $(".results")
 const fullCard = $(".full-card")
 const buttonSearch = $(".searchButton");
 const url = "https://pokeapi.co/api/v2/pokemon/";
-const sppiner = $("full-card-front")
+const articleRight = $('.full-card-front');
 
 
-buttonSearch.addEventListener("click", async (e) => {
+
+
+
+buttonSearch.addEventListener("click", async(e)=>{
+  try{
   resulTag.innerHTML = ""
   articleRight.innerHTML = `<h1> Pokemon </h1>`
   resultsSection.innerHTML = ""
   e.preventDefault();
+  if(!search.value == undefined) throw new Error("Escribe algo...")
+  
   let resultSearch = await conexionApi(url, search.value);
+  if (typeof resultSearch != "object") throw new Error(" No existe el pokemon")
   console.log(resultSearch)
   if (resultSearch.length != undefined) {
     for (let i = 0; i < resultSearch.length; i++) {
-      let pokemon = await atackApi(resultSearch[i].url);
-      
 
-      
+      let pokemon = await atackApi(resultSearch[i].url);
       console.log(pokemon)
       resultsSection.innerHTML += `
-      <article class="cards">
-        <div class='cards-container'>
+      <article class="cards" id=${pokemon.id}>
+        <div class='cards-container' >
           <div class='cards-front'>
-            <div class='img-card'>
+            <div>
               <img src=${pokemon.sprites.front_default}>  
             </div>
               <p class="cards-name"> ${pokemon.name}</p>
           </div>
           <div class='cards-back'>
             <img src=${pokemon.sprites.back_default}>
+            <p>Peso: ${pokemon.weight}</p>
+            <p>Altura: ${pokemon.height}</p>
+            <p>Order: ${pokemon.order}</p>
+            <p>Tipo: ${pokemon.types[0].type.name}</p>
+            <p>Ataque: ${pokemon.stats[1].base_stat}</p>
+            <p>Defensa: ${pokemon.stats[2].base_stat}</p>
+            <p>Velocidad: ${pokemon.stats[5].base_stat}</p>
+            <p>Puntos de vida: ${pokemon.types[0].slot}</p>
           </div>
         </div>       
-      </article>\n`
-
-      articleRight.innerHTML = `
-      <h1> Pokemon </h1>
-      <p class=parpadea> Haz click en una de las tarjetas de más abajo</p>`
-
+      </article>\n`;
 
     }
 
@@ -113,8 +152,8 @@ buttonSearch.addEventListener("click", async (e) => {
         <p>Defensa: ${resultSearch.stats[2].base_stat}</p>
         <p>Velocidad: ${resultSearch.stats[5].base_stat}</p>
         <p>Puntos de vida: ${resultSearch.types[0].slot}</p>
-        
-      </article>`
+
+      </article>`;
 
     /*  let firstGreen = document.querySelector("button-top div");
      firstGreen.classList.add(".green1");
@@ -143,9 +182,19 @@ buttonSearch.addEventListener("click", async (e) => {
 
   // </article>`
 
+      const cards = $All(".cards")
+      for(let i=0; i<cards.length; i++){
+        let id = cards[i].getAttribute("id")
+        cards[i].addEventListener("click",()=>selectCard(id))
+      }
+
   resultSearch = []
   search.value = ""
-});
+  }catch(e){
+    console.log("Error es : "+ e.message)
+  }}
+
+);
 
 
 
